a7cf11ba0759c8a64bb08c45b95ccd575cd7f309
gabrielrsh <gabriel.shiota@gmail.com>
2022-08-04 17:28:36 -0300

    criando componentes dos cursos

--- src/app/courses/courses-list.component.ts
+++ src/app/courses/courses-list.component.ts
@@ -1,9 +1,34 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 @Component({
     selector: 'app-courses-list',
-    template: '<h2> Course List </h2>'
+    templateUrl: './courses-list.component.html'
 })
-export class CoursesListComponent {
-
+export class CoursesListComponent implements OnInit{
+    courses: Course[] = [];
+    
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: "Angular: Forms",
+                imageUrl: '',
+                price: 99.99,
+                code: 'KDA-8888',
+                duration: 120,
+                rating: 5,
+                releaseDate: 'December, 2, 2019'
+            },
+            {
+                id: 2,
+                name: "Angular: HTTP",
+                imageUrl: '',
+                price: 99.99,
+                code: 'LSA-7532',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'November, 4, 2019'
+            }
+        ]
+    }
 }
