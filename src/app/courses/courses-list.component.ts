b25d1fd94b0f024cddd464be8b2d5947473aabd2
gabrielrsh <gabriel.shiota@gmail.com>
2022-08-05 15:32:13 -0300

    adiciona injeção de dependencia e eventos data binding

--- src/app/courses/courses-list.component.ts
+++ src/app/courses/courses-list.component.ts
@@ -1,34 +1,32 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
+
 @Component({
     selector: 'app-courses-list',
     templateUrl: './courses-list.component.html'
 })
 export class CoursesListComponent implements OnInit{
-    courses: Course[] = [];
+    _courses: Course[] = [];
     
+    _filterBy: string="";
+    filteredCourses: Course[] = [];
+
+    constructor(private courseService: CourseService){}
+
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: "Angular: Forms",
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'KDA-8888',
-                duration: 120,
-                rating: 5,
-                releaseDate: 'December, 2, 2019'
-            },
-            {
-                id: 2,
-                name: "Angular: HTTP",
-                imageUrl: '/assets/images/http.png',
-                price: 99.99,
-                code: 'LSA-7532',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'November, 4, 2019'
-            }
-        ]
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value:string) {
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase()
+                                                                    .indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter():string{
+        return this._filterBy
     }
 }
